e320052874b0086323103e2941c96bd5a9298011
fcsouza <fcsouza18@hotmail.com>
2020-10-23 19:17:01 -0300

    include mail config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e0181cccfae829',
+    pass: 'a57d8e7744dd4e',
+  },
+  default: {
+    from: 'Equipe Gym Point <noreplay@gympoint.com>',
+  },
+};
