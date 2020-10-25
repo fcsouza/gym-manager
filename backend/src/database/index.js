565cd3af5877bd24f56a4115f1ffe406efcb9eb2
fcsouza <fcsouza18@hotmail.com>
2020-10-25 19:47:16 -0300

    include db main

---
+++ backend/src/database/index.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+
+import User from '../app/models/User';
+import Student from '../app/models/Student';
+import Plan from '../app/models/Plan';
+import Registration from '../app/models/Registration';
+import HelpOrders from '../app/models/HelpOrders';
+import Checkin from '../app/models/Checkin';
+
+const models = [User, Student, Plan, Registration, HelpOrders, Checkin];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
+  }
+}
+export default new Database();
