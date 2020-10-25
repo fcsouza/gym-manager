8c8ceea5934dee92ef907da0de3ac195ddcccc2e
fcsouza <fcsouza18@hotmail.com>
2020-10-25 19:40:14 -0300

    include migrations

---
+++ backend/src/database/migrations/20201025001027-create-studants.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
