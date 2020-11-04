5bee5f8bceef77dcbd96a0f04ce5293563c6a481
fcsouza <fcsouza18@hotmail.com>
2020-11-04 20:35:56 -0300

    include ordersAnswer controller

---
+++ backend/src/app/controllers/HelpOrdersAnswerController.js
@@ -0,0 +1,36 @@
+import HelpOrders from '../models/HelpOrders';
+import Student from '../models/Student';
+
+class HelpOrdersAnswerController {
+  async store(req, res) {
+    const { answer_id } = req.params;
+
+    const helpOrders = await HelpOrders.findOne({ where: { id: answer_id } });
+
+    if (!helpOrders) {
+      return res.json({ error: 'Help orders not founded' });
+    }
+
+    await helpOrders.update({ answer: req.body.answer, answer_at: new Date() });
+
+    return res.json(helpOrders);
+  }
+
+  /**
+   * List all help orders without answer
+   */
+  async index(req, res) {
+    const helpOrders = await HelpOrders.findAll({
+      where: { answer: null },
+      include: {
+        model: Student,
+        as: 'student',
+        attributes: ['nome', 'email'],
+      },
+    });
+
+    return res.json(helpOrders);
+  }
+}
+
+export default new HelpOrdersAnswerController();
