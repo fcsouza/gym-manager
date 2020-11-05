deef9f0e1518193754a63dfe753f9b9aac6728c4
fcsouza <fcsouza18@hotmail.com>
2020-11-05 19:46:41 -0300

    include auth routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -18,8 +18,30 @@ routes.post('/sessions', SessionController.store);
 routes.post('/students/:id/help_orders', HelpOrdersController.store);
 routes.get('/students/:id/help_orders', HelpOrdersController.index);
 
-
 routes.post('/students/:student_id/checkins', CheckinController.store);
 routes.get('/students/:student_id/checkins', CheckinController.index);
+
+routes.use(auth);
+
+routes.post('/students', StudentController.store);
+routes.get('/students', StudentController.index);
+routes.get('/students/:id', StudentController.show);
+routes.put('/students/:id', StudentController.update);
+routes.delete('/students/:id', StudentController.delete);
+
+routes.post('/plans', PlanController.store);
+routes.get('/plans', PlanController.index);
+routes.get('/plans/:id', PlanController.show);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
+
+routes.post('/registrations', RegistrationController.store);
+routes.get('/registrations', RegistrationController.index);
+routes.get('/registrations/:plan_id', RegistrationController.show);
+routes.put('/registrations/:plan_id', RegistrationController.update);
+routes.delete('/registrations/:plan_id', RegistrationController.delete);
+
+routes.get('/help_orders', HelpOrdersAnswerController.index);
+routes.post('/help_orders/:answer_id/answer', HelpOrdersAnswerController.store);
 
 export default routes;
